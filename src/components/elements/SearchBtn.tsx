53c9edbaf055dfea2f037e60b226a2ddf414dead
SyaPratama <rasyaputraq17@gmail.com>
2025-07-07 20:29:17 +0700

    change: set background-color to button search

--- src/components/elements/SearchBtn.tsx
+++ src/components/elements/SearchBtn.tsx
@@ -25,7 +25,7 @@ export default function SearchBtn() {
       <div className="w-60 h-8 relative">
         <Button
           onClick={() => ComponentHandler(true)}
-          className="w-full h-full cursor-pointer border-2 border-slate-200 text-left pl-3 rounded-2xl text-sm text-slate-400 outline-0"
+          className="w-full h-full cursor-pointer bg-white border-2 border-slate-200 text-left pl-3 rounded-2xl text-sm text-slate-400 outline-0"
         >
           Search...
           <span className="absolute top-0 right-0 bg-slate-800 h-full px-2.5 flex items-center rounded-2xl">
